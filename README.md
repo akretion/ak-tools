4d4327dea6608c434900593baeda6a5b90f84a3b
Raphaël Valyi <rvalyi@gmail.com>
2013-02-03 02:12:01 -0200

    changed meta-data/doc

---
+++ README.md
@@ -0,0 +1,12 @@
+Description
+===========
+
+This cookbook includes Akretion base infrasture recipes
+
+Recipes
+=======
+
+server
+------
+
+To be used in headless server environment
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
-maintainer       "Akretion"
-maintainer_email "contact@akretion.com"
-license          "All rights reserved"
-description      "Akretion tools"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
-depends          "apt"
+name              "Ak-Tools"
+maintainer        "Akretion"
+maintainer_email  "contact@akretion.com"
+license           "Apache 2.0"
+description       "Akretion tools"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.0.3"
+recipe            "ak-tools::server", "Used on servers"
+depends           "apt"
