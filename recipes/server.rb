083f27175245a0c2231840449cb0b3a9ffb286aa
Sebastien BEAU <sebastien.beau@akretion.com>
2013-03-02 00:09:05 +0100

    add default[:ak_tools][:bash_aliases] for extending easily aliases for all user

--- recipes/server.rb
+++ recipes/server.rb
@@ -15,4 +15,27 @@ template "/etc/motd" do
   variables :product_name => node[:ak_tools][:product_name]
 end
 
+
+
+script "configure bashrc for all profile" do
+  interpreter "ruby"
+  user "root"
+  group "root"
+  code <<-EOH
+    File.open("/etc/bash.bashrc", 'a') do |f|
+      f.puts("if [ -f /etc/bash_aliases ]; then . /etc/bash_aliases; fi")
+    end
+    EOH
+  not_if "grep 'bash_aliases' /etc/bash.bashrc", :user => "root", :group => "root"
+end
+
+template "/etc/bash_aliases" do
+  owner "root"
+  group "root"
+  mode 00777
+  source "bash_aliases.erb"
+  variables :bash_aliases => node[:ak_tools][:bash_aliases]
+end
+
+
 (node[:ak_tools][:apt_packages] + node[:ak_tools][:apt_packages_extra]).each {|pack| package pack}
